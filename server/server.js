7bc7f8f9a766c58ab434320e2e76d3f25bc195c1
Rob Mastronicola <arejaycola@gmail.com>
2020-04-02 12:51:56 -0400

    Made some tweaks to serve static files in production

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { searchForVerifiedUser } = require('./twitterHelper');
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+const root = path.join(__dirname, '..', 'client', 'build');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,6 +15,8 @@ app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
+
+app.use('/', express.static(path.join(__dirname, '../client/build')));
 
 app.post('/twitter/search', async (req, res) => {
 	let searchString = req.body.searchString;
@@ -24,6 +27,11 @@ app.post('/twitter/search', async (req, res) => {
 app.get('/api/hello', (req, res) => {
 	res.send({ express: 'Hello From Express' });
 });
+
+// app.get('/', (req, res) => {
+// 	console.log(path.join(__dirname, '../client/build'));
+// 	res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+// });
 
 app.post('/api/world', (req, res) => {
 	console.log(req.body);
