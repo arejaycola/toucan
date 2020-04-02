1f2272e76f1f4b9e2af2d0365843c56d5ba331a1
Rob Mastronicola <arejaycola@gmail.com>
2020-04-02 13:02:50 -0400

    Added cors package

--- server/server.js
+++ server/server.js
@@ -3,18 +3,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { searchForVerifiedUser } = require('./twitterHelper');
-
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const root = path.join(__dirname, '..', 'client', 'build');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-});
+// app.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// 	next();
+// });
 
 app.use('/', express.static(path.join(__dirname, '../client/build')));
 
