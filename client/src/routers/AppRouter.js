7be46c704423fc30d39d49a41e51d402b3062311
Rob Mastronicola <arejaycola@gmail.com>
2020-04-02 13:51:33 -0400

    Added user route

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -4,8 +4,8 @@ import { createBrowserHistory } from 'history';
 import NotFoundPage from '../components/NotFoundPage';
 import HomePage from '../components/HomePage';
 import SearchResultsPage from '../components/SearchResultsPage';
+import UserPage from '../components/UserPage';
 import PublicRoute from './PublicRoute';
-import SearchContext from '../contexts/SearchContext';
 import SearchContextProvider from '../contexts/SearchContext';
 
 export const history = createBrowserHistory();
@@ -18,6 +18,8 @@ const AppRouter = () => {
 					<SearchContextProvider>
 						<PublicRoute path="/" component={HomePage} exact={true} />
 						<PublicRoute path="/search-results" component={SearchResultsPage} exact={true} />
+						<PublicRoute path="/user/" component={UserPage} exact={true} />
+						<PublicRoute path="/user/:id" component={UserPage} exact={true} />
 					</SearchContextProvider>
 					<Route component={NotFoundPage} />
 				</Switch>
@@ -27,33 +29,3 @@ const AppRouter = () => {
 };
 
 export default AppRouter;
-// import React, { useContext, useState } from 'react';
-// import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
-// import { createBrowserHistory } from 'history';
-// import NotFoundPage from '../components/NotFoundPage';
-// import HomePage from '../components/HomePage';
-// import SearchResultsPage from '../components/SearchResultsPage';
-// import PublicRoute from './PublicRoute';
-// import SearchContext from '../context/search-context'
-
-// export const history = createBrowserHistory();
-
-// const AppRouter = () => {
-// 	const [searchResults, setSearchResults] = useState();
-
-// 	return (
-// 		<Router history={history}>
-// 			<div>
-// 				<Switch>
-// 					<SearchContext.Provider value={{ searchResults, setSearchResults }}>
-// 						<PublicRoute path="/search-results" component={SearchResultsPage} exact={true} />
-// 						<PublicRoute path="/" component={HomePage} exact={true} />
-// 					</SearchContext.Provider>
-// 					<Route component={NotFoundPage} />
-// 				</Switch>
-// 			</div>
-// 		</Router>
-// 	);
-// };
-
-// export default AppRouter;
