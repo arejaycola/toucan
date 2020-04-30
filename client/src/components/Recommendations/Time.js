3541ca7ec7f852e1b1baea6328fc1329e7623b47
Rob Mastronicola <arejaycola@gmail.com>
2020-04-30 15:36:58 -0400

    Best hour is now loading correctly

--- client/src/components/Recommendations/Time.js
+++ client/src/components/Recommendations/Time.js
@@ -3,20 +3,21 @@ import moment from 'moment';
 import { Container, Col, Row } from 'react-bootstrap';
 import { TweetContext } from '../../contexts/TweetContext';
 import Loader from 'react-loader-spinner';
+import { LoadingContext } from '../../contexts/LoadingContext';
 
 const Time = () => {
-	const [maxHour, setMaxHour] = useState(-1);
+	const { isTweetsLoading, isRetweetsLoading, isQuotedTweetsLoading } = useContext(LoadingContext);
+	const { globalUnverifiedHourCount } = useContext(TweetContext);
 
 	/* TODO (04/30/2020 11:54) Somehow factor in response time.*/
-	const { globalUnverifiedHourCount } = useContext(TweetContext);
-	useEffect(() => {
-		const globalCount = globalUnverifiedHourCount;
-		const max = globalCount.indexOf(Math.max(...globalCount));
-		// if(max > 0){
-		// 	setMaxHour(max);
-		// 	console.log(maxHour);
+	const [maxHour, setMaxHour] = useState(-1);
 
-		// }
+	useEffect(() => {
+		if (!isTweetsLoading && !isRetweetsLoading && !isQuotedTweetsLoading) {
+			const globalCount = globalUnverifiedHourCount;
+			const max = globalCount.indexOf(Math.max(...globalCount));
+			setMaxHour(max);
+		}
 	}, [globalUnverifiedHourCount]);
 
 	const bestHour = moment().set('hour', maxHour).set('minute', 0).format('HH:mm A');
