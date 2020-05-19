8688a1c81d06375b97765ef55d6266ab60971c20
Rob Mastronicola <arejaycola@gmail.com>
2020-05-19 15:30:45 -0400

    Reordered elements

--- client/src/components/Recommendations/Recommendations.js
+++ client/src/components/Recommendations/Recommendations.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Col, Row, Button } from 'react-bootstrap';
 import DayAndTime from './DayAndTime';
 import Time from './Time';
 import TimeToday from './TimeToday';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 const Recommendations = () => {
+	const onSettingsClick = () => {
+		console.log('tehe');
+	};
+
 	return (
-		<Row>
-			<Col>
-				<h3>Recommendations</h3>
-				<Time />
-				<TimeToday />
-				<DayAndTime />
-			</Col>
-		</Row>
+		<>
+			<Row className="mb-4">
+				<Col>
+					<h3>
+						Recommendations
+						<a style={{ cursor: 'pointer' }}>
+							<FontAwesomeIcon onClick={onSettingsClick} style={{ marginLeft: '10px' }} icon={faCog} />
+						</a>
+					</h3>
+				</Col>
+			</Row>
+			<Row>
+				<Col className="mb-3" sm={4}>
+					<DayAndTime />
+				</Col>
+				<Col className="mb-3" sm={4}>
+					<TimeToday />
+				</Col>
+				<Col className="mb-3" sm={4}>
+					<Time />
+				</Col>
+			</Row>
+		</>
 	);
 };
 
