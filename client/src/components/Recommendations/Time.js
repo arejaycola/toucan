507bdadb87314ff437557fb4a4af04279516390c
Rob Mastronicola <arejaycola@gmail.com>
2020-05-21 14:48:38 -0400

    Graphing best time overall now

--- client/src/components/Recommendations/Time.js
+++ client/src/components/Recommendations/Time.js
@@ -1,24 +1,49 @@
 import React, { useContext, useState, useEffect } from 'react';
 import moment from 'moment';
-import {  Col, Row, Button } from 'react-bootstrap';
+import { Col, Row, Button } from 'react-bootstrap';
 import { TweetContext } from '../../contexts/TweetContext';
 import Loader from 'react-loader-spinner';
 import { LoadingContext } from '../../contexts/LoadingContext';
+import D3Chart from '../charts/D3Chart';
 
-const Time = ({onViewClick}) => {
+const Time = ({ onViewClick }) => {
 	const { isTweetsLoading, isRetweetsLoading, isQuotedTweetsLoading } = useContext(LoadingContext);
-	const { globalUnverifiedHourCount } = useContext(TweetContext);
+	const { globalUnverifiedHourCount, globalVerifiedHourCount } = useContext(TweetContext);
 
 	/* TODO (04/30/2020 11:54) Somehow factor in response time.*/
 	const [maxHour, setMaxHour] = useState(-1);
+	const [hoursForGraphing, setHoursForGraphing] = useState(Array(24).fill(0));
+	const [showChart, setShowChart] = useState(false);
+
+	const hourTickFormat = (d) => {
+		if (d === 12) {
+			return '12 pm';
+		} else if (d === 0) {
+			return '12am';
+		}
+
+		return moment().hour(d).format('h');
+	};
+
+	const onToggleViewClick = () => {
+		setShowChart(!showChart);
+	};
 
 	useEffect(() => {
 		if (!isTweetsLoading && !isRetweetsLoading && !isQuotedTweetsLoading) {
-			const globalCount = globalUnverifiedHourCount;
-			const max = globalCount.indexOf(Math.max(...globalCount));
+			console.log(globalUnverifiedHourCount);
+			console.log(globalVerifiedHourCount);
+
+			const tempArray = Array(24).fill(0);
+			for (let i = 0; i < globalVerifiedHourCount.length; i++) {
+				tempArray[i] = globalVerifiedHourCount[i] + globalUnverifiedHourCount[i];
+			}
+
+			setHoursForGraphing(tempArray);
+			const max = tempArray.indexOf(Math.max(...tempArray));
 			setMaxHour(max);
 		}
-	}, [globalUnverifiedHourCount]);
+	}, [globalUnverifiedHourCount, globalVerifiedHourCount]);
 
 	const bestHour = moment().set('hour', maxHour).set('minute', 0).format('hh:mm A');
 
@@ -37,9 +62,20 @@ const Time = ({onViewClick}) => {
 				<Row>
 					<Col>Best hour to tweet (on average)</Col>
 				</Row>
-				<Row className='mt-1'>
+				<Row className="mt-1">
 					<Col>
-						<Button id="time" onClick={(e) => onViewClick("time")}>View</Button>
+						<Button id="time-today" onClick={onToggleViewClick}>
+							{showChart ? 'Hide' : 'View'}
+						</Button>
+
+						{showChart ? (
+							<D3Chart
+								id="d3-time-today-chart"
+								label="# of Statuses"
+								tickFormat={hourTickFormat}
+								data={[{ type: 'dark-gray', datum: hoursForGraphing }]}
+							/>
+						) : null}
 					</Col>
 				</Row>
 			</Col>
