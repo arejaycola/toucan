f03a20bcaca7cbaea5fac383771b72b026d90098
Rob Mastronicola <arejaycola@gmail.com>
2020-06-11 15:30:25 -0400

    Checkboxes now represent the graph legend

--- client/src/components/Recommendations/Filters.js
+++ client/src/components/Recommendations/Filters.js
@@ -12,18 +12,31 @@ const Filters = ({ toggleStatus, toggleUserType, showAllStatuses, showTweets, sh
 			<Row className="justify-content-center">
 				<Col xs={'auto'} className="text-left">
 					<h5>Status Type</h5>
-					<Form.Check type="switch" checked={showAllStatuses} id="show-all-status" label="All" onChange={(e) => toggleStatus(e)} />
-					<Form.Check type="switch" checked={showTweets} id="show-tweets" label="Tweets" onChange={(e) => toggleStatus(e)} />
-					<Form.Check type="switch" checked={showRetweets} id="show-retweets" label="Retweets" onChange={(e) => toggleStatus(e)} />
-					
+					<FormCheck onChange={(e) => toggleStatus(e)} id="show-all-status" type="switch" custom>
+						<FormCheck.Input readOnly id="show-all-status" checked={showAllStatuses} />
+						<FormCheck.Label id="show-all-status" onClick={(e) => toggleStatus(e)}>
+							All
+						</FormCheck.Label>
+					</FormCheck>
+					<FormCheck onChange={(e) => toggleStatus(e)} id="show-tweets" type="switch" custom>
+						<FormCheck.Input readOnly id="show-tweets" checked={showTweets} />
+						<FormCheck.Label id="show-tweets" onClick={(e) => toggleStatus(e)}>
+							Tweets
+						</FormCheck.Label>
+					</FormCheck>
+					<FormCheck onChange={(e) => toggleStatus(e)} id="show-retweets" type="switch" custom>
+						<FormCheck.Input readOnly id="show-retweets" checked={showRetweets} />
+						<FormCheck.Label id="show-retweets" onClick={(e) => toggleStatus(e)}>
+							Retweets
+						</FormCheck.Label>
+					</FormCheck>
+
 					<FormCheck onChange={(e) => toggleStatus(e)} id="show-quoted-tweets" type="switch" custom>
 						<FormCheck.Input readOnly id="show-quoted-tweets" checked={showQuotedTweets} />
 						<FormCheck.Label id="show-quoted-tweets" onClick={(e) => toggleStatus(e)}>
 							Quoted Tweets
 						</FormCheck.Label>
 					</FormCheck>
-
-				
 				</Col>
 				<Col xs={'auto'} className="text-left">
 					<h5>User Type</h5>
